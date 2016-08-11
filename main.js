51ba9012b650f8ab4f99989b1dcf64a008c6bdd9
Roger Camargo <huogerac@gmail.com>
2016-08-11 16:57:41 -0300

    Initial version

---
+++ main.js
@@ -0,0 +1,90 @@
+define(['HubLink', 'RIB', 'PropertiesPanel', 'Easy'], function(Hub, RIB, Ppanel, easy) {
+  var actions = ["ACTION1", "ACTION2"];
+  var inputs = [];
+  var _objects = {};
+  var {{ cookiecutter.block_shortname }} = {
+    settings:{
+      Custom: {}
+    },
+    dataFeed: {}
+  };
+
+  // TODO: Review if this is a trully unique instance?
+
+  {{ cookiecutter.block_shortname }}.getActions = function() {
+    return actions;
+  };
+
+  {{ cookiecutter.block_shortname }}.getInputs = function() {
+    return inputs;
+  };
+
+  /**
+   * Triggered when added for the first time to the side bar.
+   * This script should subscribe to all the events and broadcast
+   * to all its copies the data.
+   * NOTE: The call is bind to the block's instance, hence 'this'
+   * does not refer to this module, for that use '{{ cookiecutter.block_shortname }}'
+   */
+  {{ cookiecutter.block_shortname }}.onLoad = function(){
+
+  };
+
+  /**
+   * Allows blocks controllers to change the content
+   * inside the Logic Maker container
+   */
+  {{ cookiecutter.block_shortname }}.lmContentOverride = function(){
+    // Use this to inject your custom HTML into the Logic Maker screen.
+    return "<div> {{ cookiecutter.block_shortname }} html </div>";
+  };
+
+  /**
+   * Parent is asking me to execute my logic.
+   * This block only initiate processing with
+   * actions from the hardware.
+   */
+  {{ cookiecutter.block_shortname }}.onExecute = function() {
+
+
+  };
+
+  // TODO: Move this to the block controller
+  function save() {
+
+  }
+
+  /**
+   * Triggered when the user clicks on a block.
+   * The interace builder is automatically opened.
+   * Here we must load the elements.
+   * NOTE: This is called with the scope set to the
+   * Block object, to emailsess this modules properties
+   * use {{ cookiecutter.block_shortname }} or this.controller
+   */
+  {{ cookiecutter.block_shortname }}.onClick = function(){
+
+  };
+
+  /**
+   * Parent is send new data (using outputs).
+   */
+  {{ cookiecutter.block_shortname }}.onNewData = function() {};
+
+  // Returns the current value of my inputs
+  // {{ cookiecutter.block_shortname }}.onRead = function(){};
+
+  // Optional event handlers
+  {{ cookiecutter.block_shortname }}.onMouseOver = function(){
+    // console.log("Mouse Over on ", myself.canvasIcon.id, evt);
+  };
+
+  /**
+   * A copy has been dropped on the canvas.
+   * I need to keep a copy of the processor to be triggered when
+   * new data arrives.
+   */
+  {{ cookiecutter.block_shortname }}.onAddedtoCanvas = function(){};
+
+  return {{ cookiecutter.block_shortname }};
+});
