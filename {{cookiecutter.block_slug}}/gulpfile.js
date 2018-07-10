201f761e690c81a5c0c2fde559407f5127fe57a3
Alexander Agudelo <mario323qp@gmail.com>
2018-07-10 15:36:46 +1000

    Added gulp task to produce .asar bundles

--- {{cookiecutter.block_slug}}/gulpfile.js
+++ {{cookiecutter.block_slug}}/gulpfile.js
@@ -1,58 +1,79 @@
-var gulp = require('gulp'),
-    utils = require('gulp-util'),
-    jshint = require('gulp-jshint'),
-    pump = require('pump');
-    uglify = require('gulp-uglify'),
-    uglifycss = require('gulp-uglifycss'),
-    htmlmin = require('gulp-htmlmin'),
-    babel = require('gulp-babel');
-
+var gulp = require("gulp"),
+  utils = require("gulp-util"),
+  jshint = require("gulp-jshint"),
+  pump = require("pump"),
+  uglify = require("gulp-uglify"),
+  uglifycss = require("gulp-uglifycss"),
+  htmlmin = require("gulp-htmlmin"),
+  babel = require("gulp-babel"),
+  gulpAsar = require("gulp-asar"),
+  del = require("del");
 
 var dest = "dist/";
 
 // Check the code quality
-gulp.task('qualitychecker', function(cb) {
-	return gulp.src([
-		'**/*.js',
-		'!node_modules/**/*.js',
-		'!dist/**/*.js',
-		'!{{cookiecutter.block_shortname}}/components/**/*.js'])
-		.pipe(jshint({esversion: 6}))
-		.pipe(jshint.reporter('default'))
-		.on('error', utils.log);
+gulp.task("qualitychecker", function(cb) {
+  return gulp
+    .src([
+      "**/*.js",
+      "!node_modules/**/*.js",
+      "!dist/**/*.js",
+      "!{{cookiecutter.block_shortname}}/components/**/*.js"
+    ])
+    .pipe(jshint({ esversion: 6 }))
+    .pipe(jshint.reporter("default"))
+    .on("error", utils.log);
 });
 
-
-gulp.task('js', function (cb) {
-  pump([
-    gulp.src('{{cookiecutter.block_shortname}}/**/*.js'),
-    babel({"presets": ["env"]}),
-    uglify(),
-    gulp.dest(dest)
+gulp.task("js", function(cb) {
+  pump(
+    [
+      gulp.src("{{cookiecutter.block_shortname}}/**/*.js"),
+      babel({ presets: ["env"] }),
+      uglify(),
+      gulp.dest(dest)
     ],
     cb
   );
 });
 
-
-gulp.task('css', function(cb) {
-	pump([
-		gulp.src('{{cookiecutter.block_shortname}}/**/*.css'),
-		uglifycss(),
-		gulp.dest(dest)
-	], cb);
-});
-
-
-gulp.task('html', function (cb) {
-  pump([
-    gulp.src('{{cookiecutter.block_shortname}}/**/*.html'),
-    htmlmin({collapseWhitespace: true}),
-    gulp.dest(dest)
+gulp.task("css", function(cb) {
+  pump(
+    [
+      gulp.src("{{cookiecutter.block_shortname}}/**/*.css"),
+      uglifycss(),
+      gulp.dest(dest)
     ],
     cb
   );
 });
 
-gulp.task('build', ['js', 'css', 'html']);
-gulp.task('default', ['qualitychecker']);
+gulp.task("html", function(cb) {
+  return pump(
+    [
+      gulp.src("{{cookiecutter.block_shortname}}/**/*.html"),
+      htmlmin({ collapseWhitespace: true }),
+      gulp.dest(dest)
+    ],
+    cb
+  );
+});
+
+gulp.task("clean", function(cb) {
+  return del([dest + "/installer.asar"]);
+});
+
+gulp.task("bundle", function(cb) {
+  return pump([
+    gulp.src([dest + "/**", "!/installer.asar"]),
+    gulpAsar("installer.asar"),
+    gulp.dest(dest)
+  ]);
+});
+
+gulp.task(
+  "build",
+  gulp.series("clean", gulp.parallel("js", "css", "html"), "bundle")
+);
+
+gulp.task("default", gulp.series("qualitychecker"));
